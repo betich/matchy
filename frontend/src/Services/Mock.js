030022156bedef2e161d571ee6c7946ab28c0289
Panithi Makthiengtrong <betich365@gmail.com>
2021-01-30 03:26:20 +0700

    add mock data fr

---
+++ frontend/src/Services/Mock.js
@@ -0,0 +1,4 @@
+const ProjectTags = ['Transportation', 'Tourism', 'Technology', 'Supplier', 'Subscription', 'Store', 'Sports', 'Software', 'Pet', 'Organization', 'Logistics', 'Insurance', 'Health', 'Games', 'Food', 'Fintech', 'Farm', 'Environment', 'Entertainment', 'Education', 'eCommerce', 'Delivery', 'Dealer', 'Construction', 'Cafe', 'Beauty', 'Audit', 'Art', 'Animal', 'AI', 'Agency'];
+const Interests = ['Website Design', 'Video Editing', 'Photography', 'Audio Engineering', 'E-Sports'];
+
+export { ProjectTags, Interests }; 
