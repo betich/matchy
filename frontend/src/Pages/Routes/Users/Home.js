93bf69bd58750bbd46fa8acd76f8ac40905a2cc6
ImSoZRious <pannawichtata@gmail.com>
2021-02-06 00:44:49 +0700

    Change user home page

--- frontend/src/Pages/Routes/Users/Home.js
+++ frontend/src/Pages/Routes/Users/Home.js
@@ -1,23 +1,39 @@
-import { Button } from 'react-bootstrap';
+import { Button, Card } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Loading from '../../../Components/Loading';
 
-const UserLink = (props) => {
-    return (
-        <Link to={`/users/${props.url}`}>
-            <Button variant="outline-info" type="submit">
-                {props.name}
-            </Button>
-        </Link>
+const UserCard = (props) => {
+return ( 
+    <>
+        <Card
+            bg="white"
+            text="black"
+            style={{ width: '18rem' }}
+            className="mb-2"
+            >
+            <Card.Header>{props.name}</Card.Header>
+            <Card.Body>
+                <Card.Title>{props.fullname}</Card.Title>
+                <Card.Text>
+
+                </Card.Text>
+            </Card.Body>
+            <Link to={`/users/${props.url}`} className="d-flex justify-content-center mb-3">
+                <Button variant="outline-info" type="submit">
+                    View
+                </Button>
+            </Link>
+        </Card>
+    </>
     );
 }
 
 const Index = (props) => {
     const [Users, setUsers] = useState([]);
     const [loaded, setLoad] = useState(false);
-    let UserLinks = Users.map((e) => <UserLink url={e._id} name={e.username} key={e._id} />);
+    let UserCardList = Users.map((e) => <UserCard url={e._id} name={e.username} key={e._id} fullname={e.name.first + " " + e.name.last}/>);
 
     useEffect(() => {
         axios
@@ -25,6 +41,7 @@ const Index = (props) => {
         .then((res) => res.data)
         .then((users) => {
             setUsers(users);
+            console.log(users)
         })
         .then(() => setLoad(true))
         .catch((err) => console.error("oh no", err))
@@ -52,7 +69,9 @@ const Index = (props) => {
             </Link>
             <div id="projectLinks">
                 <h4>Project Links</h4>
-                {UserLinks}
+                <div className="d-flex flex-wrap">
+                    {UserCardList}
+                </div>
             </div>
         </>
         )
