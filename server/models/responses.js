3f66ae1d14bb958fab06c7ac2acba8953d10ee96
bTch <betich365@gmail.com>
2021-02-23 19:44:32 +0700

    add response model

---
+++ server/models/responses.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const responseSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId, ref: 'User'
+    },
+    project: {
+        type: mongoose.Schema.Types.ObjectId, ref: 'Project'
+    },
+    answers: [{ type: Object }]
+})
+
+module.exports = mongoose.model("Response", responseSchema);
